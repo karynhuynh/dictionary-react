63d67a075b4b4deaa205f5e50b44c40ae6ae00c3
karynhuynh <79120906+karynhuynh@users.noreply.github.com>
2021-04-29 12:14:48 -0700

    Integrated axios and the dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function wordSubmit(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    // documentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function wordSearch(event) {
