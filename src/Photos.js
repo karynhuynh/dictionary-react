4cbde56db04eb64104a92c526fa2eb3a37936fdc
karynhuynh <79120906+karynhuynh@users.noreply.github.com>
2021-05-02 11:46:05 -0700

    Displaying the API images through a new photos component

---
+++ src/Photos.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+  console.log(props.photos);
+  if (props.photos) {
+    return (
+      <section className="Photos">
+        <div className="row">
+          {props.photos.map((photo, index) => {
+            console.log(photo);
+            return (
+              <div className="col-6" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img src={photo.src.landscape} alt='corresponding pexels to keyword input'
+                  className="img-fluid" />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
